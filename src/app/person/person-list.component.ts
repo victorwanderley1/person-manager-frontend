bd415730280c51b91b868f33e006344eb9a1abc0
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-07-28 15:04:22 -0300

    Modify PersonListComponents

--- src/app/person/person-list.component.ts
+++ src/app/person/person-list.component.ts
@@ -5,34 +5,61 @@ import { PersonService } from "./person.service";
 @Component({
     templateUrl: './person-list.component.html'
 })
-export class PersonListComponent implements OnInit{
+export class PersonListComponent implements OnInit {
     _persons: Person[] = [];
     filtredPersons: Person[] = [];
     _filterBy!: string;
 
 
-    constructor(private service: PersonService){}
+    constructor(private service: PersonService) { }
     ngOnInit(): void {
         this.findAll();
     }
 
-    findAll(): void{
+    findAll(): void {
         this.service.findAll().subscribe({
             next: person => {
                 this._persons = person;
-                this.filtredPersons = this._persons;
-            },
+        this.filtredPersons = this._persons;
+            }, 
             error: err => console.log('Error', err)
         });
+        // this._persons = PERSONS;
+        // this.filtredPersons = this._persons;
     }
 
-    set filter(value: string){
+    deleteById(id: string): void { 
+        this.service.deleteById(id).subscribe({
+            next: () => { 
+                console.log('Deleted with success');
+                this.findAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
+    set filter(value: string) {
         this._filterBy = value;
         this.filtredPersons = this._persons.filter((person: Person) => person.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
     }
 
-    get filter(){
+    get filter() {
         return this._filterBy;
     }
 
 }
+
+var PERSONS: Person[] = [
+    {
+        id: 'abc',
+        name: 'xablau',
+        email: 'xablau@gmail.com',
+        phone: '868686868'
+    },
+    {
+        id: 'def',
+        name: 'xeresvaldo',
+        email: 'xeresvaldo@gmail.com',
+        phone: '0008880'
+    }
+]
