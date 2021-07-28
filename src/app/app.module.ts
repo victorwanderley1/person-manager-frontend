34143170c19d5ea2838ad4c8c74e651cab675319
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-07-28 15:06:40 -0300

    Modify app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PersonAddComponent } from './person/person-add.component';
+import { PersonEditComponent } from './person/person-edit.component';
 import { PersonListComponent } from './person/person-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonListComponent
+    PersonListComponent,
+    PersonEditComponent,
+    PersonAddComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'persons', pathMatch: 'full'
       },
       {
         path: 'persons', component: PersonListComponent
+      },
+      {
+        path: 'persons/edit/:id', component: PersonEditComponent
+      },
+      {
+        path: 'persons/new', component: PersonAddComponent
       }
     ])
   ],
